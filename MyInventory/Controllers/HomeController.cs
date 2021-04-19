82e10db4ddec9bd19f5434dedd78724be6b70a66
anekieltrinidad <tanekiel@gmail.com>
2021-04-19 18:57:16 +0800

    Updated email notification

--- MyInventory/Controllers/HomeController.cs
+++ MyInventory/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyInventory.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace MyInventory.Controllers
 {
@@ -32,6 +34,43 @@ namespace MyInventory.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact (Contact record)
+        {
+            using (MailMessage mail = new MailMessage("benilde.web.development@gmail.com", record.email))
+            {
+                mail.Subject = record.Subject;
+
+                string message = "Hello, " + record.SenderName + "!<br/><br/>" +
+                    "We have received your inquiry. Here are the details: <br/><br/>" +
+                    "Contact Number: <strong>" + record.ContactNo + "</strong><br/>" +
+                    "Message:<br/><strong>" + record.Message + "</strong><br/><br/>" +
+                    "Please wait for our reply. Thank you!";
+                mail.Body = message;
+                mail.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred = new NetworkCredential("benilde.web.development@gmail.com", "Athisisalongpassword1234");
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mail);
+                    ViewBag.Message = "Inquiry Sent";
+
+                }
+            }
+            return View();
+
         }
     }
 }
---
+++ MyInventory/Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyInventory.Models
+{
+    public class Contact
+    {
+
+        [Display(Name = "Sender Name")]
+        [Required(ErrorMessage = "Required")]
+        public String SenderName { get; set; }
+
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Format")]
+        [Required(ErrorMessage = "Required")]
+        public String email { get; set; }
+
+        [Display(Name = "Contact Number")]
+        public String ContactNo { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public String Subject { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [DataType(DataType.MultilineText)]
+        public String Message { get; set; }
+    }
+}
